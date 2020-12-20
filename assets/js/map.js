3e262b18ad2e2c087b765947a382196fc08a73c1
Christopher Straub <ccstraub@gmail.com>
2020-12-19 22:35:25 -0600

    implemented custom map with leaflet and mapbox

---
+++ assets/js/map.js
@@ -0,0 +1,23 @@
+// The latitude and longitude coordinates 29.785786, -95.824394 correspond to Katy, Texas.
+var mymap = L.map('map').setView([29.785786, -95.824394], 11);
+
+L.tileLayer(
+  'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2NzdHJhdWIiLCJhIjoiY2tpd2x4dGxiMGV4NjMwb2RuaXd2a3lqeCJ9.QGoQW28PEf4tCnf_GefGdw',
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken:
+      'pk.eyJ1IjoiY2NzdHJhdWIiLCJhIjoiY2tpd2x4dGxiMGV4NjMwb2RuaXd2a3lqeCJ9.QGoQW28PEf4tCnf_GefGdw',
+  }
+).addTo(mymap);
+
+var circle = L.circle([29.785786, -95.824394], {
+  color: 'purple',
+  fillColor: 'purple',
+  fillOpacity: 0.25,
+  radius: 2000,
+}).addTo(mymap);
